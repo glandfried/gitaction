5c95d75ca1fda4230728eedb365bb6540f7cb683
Gustavo Landfried <gustavolandfried@gmail.com>
2020-01-10 14:15:32 -0500

    Add setup script of git-lsf

---
+++ setup/lfs-install.sh
@@ -0,0 +1,2 @@
+wget https://github.com/git-lfs/git-lfs/releases/download/v2.9.2/git-lfs-linux-amd64-v2.9.2.tar.gz -P download/
+tar xvzf download/git-lfs-linux-amd64-v2.9.2.tar.gz -C download/git-lfs-linux-amd64-v2.9.2/ 
