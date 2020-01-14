e391766428fd0e6c3e9c7b9acd5d9236d53eab0a
Gustavo Landfried (Mininet) <gustavolandfried@gmail.com>
2020-01-14 12:15:14 -0300

    Change lfs-install.sh

--- setup/lfs-install.sh
+++ setup/lfs-install.sh
@@ -1,2 +1,4 @@
-wget https://github.com/git-lfs/git-lfs/releases/download/v2.9.2/git-lfs-linux-amd64-v2.9.2.tar.gz -P download/
-tar xvzf download/git-lfs-linux-amd64-v2.9.2.tar.gz -C download/git-lfs-linux-amd64-v2.9.2/ 
+wget https://github.com/git-lfs/git-lfs/releases/download/v2.9.2/git-lfs-linux-amd64-v2.9.2.tar.gz -P /var/tmp/
+mkdir /var/tmp/git-lfs-linux-amd64-v2.9.2/ 
+tar xvzf /var/tmp/git-lfs-linux-amd64-v2.9.2.tar.gz -C /var/tmp/git-lfs-linux-amd64-v2.9.2/ 
+sudo sh /var/tmp/git-lfs-linux-amd64-v2.9.2/install.sh
