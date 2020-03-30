1618e04edaf30abbd9c7f9a63c5aabebe1902b25
Gustavo Landfried <gustavolandfried@gmail.com>
2020-03-30 16:49:44 -0300

    Add mirror

---
+++ git-mirror.sh
@@ -0,0 +1,6 @@
+# If you just want to track changes, first make an empty repository in GitLab (or whatever else you may be using) and clone it to your computer.
+# git remote add upstream https://github.com/user/repo
+# git pull upstream master
+# git checkout -b mirror
+# git push -u origin mirror
+
