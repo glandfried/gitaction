f19c83be6bf2ef71bea66d1ff35c783954f6e9d6
Gustavo Landfried <gustavolandfried@gmail.com>
2022-02-15 17:44:25 -0300

    Algo de como subir paquetes a pip

---
+++ python.sh
@@ -0,0 +1,9 @@
+#Cambiar version en setup.py
+#Crear tag con esa versión
+#https://pypi.org/manage/project/trueskillthroughtime/releases/
+#https://packaging.python.org/en/latest/tutorials/packaging-projects/
+python3 -m build
+#This command should output a lot of text and once completed should generate two files in the dist directory:
+#dist/
+#  example-package-YOUR-USERNAME-HERE-0.0.1-py3-none-any.whl
+#  example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz
